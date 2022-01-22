3cd2162990432729ff2f81225c248f92ab167d2b
Anthony Robertson <anthony.robertson@shopify.com>
2022-01-22 15:11:56 -0500

    adding the food delete mutation

--- app/graphql/types/mutation_type.rb
+++ app/graphql/types/mutation_type.rb
@@ -1,5 +1,6 @@
 module Types
   class MutationType < Types::BaseObject
+    field :food_delete, mutation: Mutations::FoodDelete
     field :nutrition_update, mutation: Mutations::NutritionUpdate
     field :food_update, mutation: Mutations::FoodUpdate
     field :nutrition_create, mutation: Mutations::NutritionCreate
