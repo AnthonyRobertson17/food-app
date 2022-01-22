66b3bae2f28be0b20de6c71f308a336b9cba3ad4
Anthony Robertson <anthony.robertson@shopify.com>
2022-01-22 14:50:06 -0500

    adding the nutrition create mutation

--- app/graphql/types/mutation_type.rb
+++ app/graphql/types/mutation_type.rb
@@ -1,5 +1,6 @@
 module Types
   class MutationType < Types::BaseObject
+    field :nutrition_create, mutation: Mutations::NutritionCreate
     field :food_create, mutation: Mutations::FoodCreate
   end
 end
