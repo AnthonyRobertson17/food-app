f6df5aca474eae267614d9df1174cfe25e8d210e
Anthony Robertson <anthony.robertson@shopify.com>
2022-01-22 14:25:06 -0500

    adding nutrition field to food type

--- app/graphql/types/food_type.rb
+++ app/graphql/types/food_type.rb
@@ -6,5 +6,6 @@ module Types
     field :image, String, null: true
     field :created_at, GraphQL::Types::ISO8601DateTime, null: false
     field :updated_at, GraphQL::Types::ISO8601DateTime, null: false
+    field :nutrition, Types::NutritionType, null: true
   end
 end
