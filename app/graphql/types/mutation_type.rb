97ac21e13cbac4276eb042153a146c157e5a4895
Anthony Robertson <anthony.robertson@shopify.com>
2022-01-22 15:05:48 -0500

    adding the food update mutation

--- app/graphql/types/mutation_type.rb
+++ app/graphql/types/mutation_type.rb
@@ -1,5 +1,6 @@
 module Types
   class MutationType < Types::BaseObject
+    field :food_update, mutation: Mutations::FoodUpdate
     field :nutrition_create, mutation: Mutations::NutritionCreate
     field :food_create, mutation: Mutations::FoodCreate
   end
